c15886cd385a17e2c4ee4a73af5093d6e274c5f2
arima-kishouuu <dan542323@gmail.com>
2025-11-18 03:47:54 +0200

    Implement basic Actix web server with a GET endpoint for pizzas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{ get, App, post , patch, Responder , HttpResponse , HttpServer};
+
+#[get("/pizzas")]
+async fn get_pizzas() -> impl Responder {
+    HttpResponse::Ok().body("Pizzas available")
+}
+
+// #[post("/pizzas")]
+// async fn create_pizza() -> impl Responder {
+//     HttpResponse::create().body("pizzacreate")
+// }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(get_pizzas)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
