0a2cf767ada38416796270adc99932c8c91643a6
arima-kishouuu <dan542323@gmail.com>
2025-11-18 04:05:14 +0200

    Add POST and PATCH endpoints for buying and updating pizzas

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,23 @@ async fn get_pizzas() -> impl Responder {
     HttpResponse::Ok().body("Pizzas available")
 }
 
-// #[post("/pizzas")]
-// async fn create_pizza() -> impl Responder {
-//     HttpResponse::create().body("pizzacreate")
-// }
+#[post("/buypizzas")]
+async fn buy_pizza() -> impl Responder {
+    HttpResponse::Ok().body("Buying a pizza!")
+}
+
+#[patch("/updatepizzas/{uuid}")]
+async fn update_pizza() -> impl Responder {
+    HttpResponse::Ok().body("pizza updated!")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(get_pizzas)
+            .service(buy_pizza)
+            .service(update_pizza)
     })
     .bind("127.0.0.1:8080")?
     .run()
